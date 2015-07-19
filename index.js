2978894e76329f1506c3a5c2538db96b8c44bdbd
Luke Horvat <lukehorvat@gmail.com>
2015-07-20 01:18:32 +1000

    Define module as an ES6 class

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-export let declarationTypes = [
-  "CLASS",
-  "CLASS_METHOD",
-  "CLASS_ACCESSOR",
-  "OBJECT_LITERAL_METHOD",
-  "OBJECT_LITERAL_ACCESSOR"
-].reduce((obj, name) => {
-  return Object.defineProperty(obj, name, { value: Symbol(name) });
-}, {});
+export class DecoratorUtils {
+  static declarationType = [
+    "CLASS",
+    "CLASS_METHOD",
+    "CLASS_ACCESSOR",
+    "OBJECT_LITERAL_METHOD",
+    "OBJECT_LITERAL_ACCESSOR"
+  ].reduce((obj, name) => {
+    return Object.defineProperty(obj, name, { value: Symbol(name) });
+  }, {})
 
-export function getDeclarationType(args) {
-  let [target, name, descriptor] = Array.slice(args);
+  static getDeclarationType(args) {
+    let [target, name, descriptor] = Array.slice(args);
 
-  if (args.length === 1 && typeof target === "function") {
-    return declarationTypes.CLASS;
-  } else if (args.length === 3 && typeof target === "object" && typeof target.constructor === "function") {
-    let isObjectLiteral = target.constructor.name === "Object";
-    let isAccessor = descriptor.get || descriptor.set;
-    return declarationTypes[`${isObjectLiteral ? "OBJECT_LITERAL" : "CLASS"}_${isAccessor ? "ACCESSOR" : "METHOD"}`];
+    if (args.length === 1 && typeof target === "function") {
+      return DecoratorUtils.declarationType.CLASS;
+    } else if (args.length === 3 && typeof target === "object" && typeof target.constructor === "function") {
+      let isObjectLiteral = target.constructor.name === "Object";
+      let isAccessor = descriptor.get || descriptor.set;
+      return DecoratorUtils.declarationType[`${isObjectLiteral ? "OBJECT_LITERAL" : "CLASS"}_${isAccessor ? "ACCESSOR" : "METHOD"}`];
+    }
+
+    return null;
   }
-
-  return null;
 };
